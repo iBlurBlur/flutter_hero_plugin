9329536f2f16276d700a19c03f6d861eb2085217
iBlurBlur <tanakorn.ngam@gmail.com>
2021-07-19 07:56:44 +0700

    revise code

--- android/src/main/kotlin/com/iblurblur/flutter_hero_plugin/ColorStreamHandler.kt
+++ android/src/main/kotlin/com/iblurblur/flutter_hero_plugin/ColorStreamHandler.kt
@@ -6,7 +6,7 @@ import io.flutter.plugin.common.EventChannel
 
 class ColorStreamHandler : EventChannel.StreamHandler {
     private var eventSink: EventChannel.EventSink? = null
-    private val color = listOf(0xff71C9CE, 0xffA6E3E9, 0xffCBF1F5, 0xffE3FDFD)
+    private val colors = listOf(0xff71C9CE, 0xffA6E3E9, 0xffCBF1F5, 0xffE3FDFD)
     private var index = 0
 
     override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
@@ -22,10 +22,10 @@ class ColorStreamHandler : EventChannel.StreamHandler {
         val handler = Handler(Looper.getMainLooper())
         handler.post(object : Runnable {
             override fun run() {
-                if (index >= color.count()) {
+                if (index >= colors.count()) {
                     index = 0
                 }
-                eventSink?.success(color[index])
+                eventSink?.success(colors[index])
                 index += 1
                 handler.postDelayed(this, 3000)
             }
--- android/src/main/kotlin/com/iblurblur/flutter_hero_plugin/ProfileViewFactory.kt
+++ android/src/main/kotlin/com/iblurblur/flutter_hero_plugin/ProfileViewFactory.kt
@@ -2,7 +2,6 @@ package com.iblurblur.flutter_hero_plugin
 
 import android.app.Activity
 import android.content.Context
-import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.LinearLayout
@@ -17,37 +16,40 @@ import io.flutter.plugin.common.StandardMessageCodec
 import io.flutter.plugin.platform.PlatformView
 import io.flutter.plugin.platform.PlatformViewFactory
 
-class ProfileViewFactory(private val registrar: BinaryMessenger) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
-    override fun create(context: Context, viewId: Int, args: Any?): PlatformView {
-        val creationParams = args as Map<String?, Any?>
-        return NativeView(registrar, context, viewId, creationParams)
+class ProfileViewFactory(private val messenger: BinaryMessenger) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+    override fun create(context: Context, viewIdentifier: Int, args: Any?): PlatformView {
+        val arguments = args as Map<String?, Any?>
+        return ProfileView(context, viewIdentifier, arguments, messenger)
     }
 }
 
-internal class NativeView(registrar: BinaryMessenger, context: Context, viewId: Int, creationParams: Map<String?, Any?>?) : PlatformView, MethodChannel.MethodCallHandler, ActivityAware {
-    private val parentLinearLayout: LinearLayout = LinearLayout(context)
-    private var nameTextView: TextView
+internal class ProfileView(context: Context, viewId: Int, args: Map<String?, Any?>?, messenger: BinaryMessenger) : PlatformView, MethodChannel.MethodCallHandler, ActivityAware {
     private lateinit var activity: Activity
-    private val channel = MethodChannel(registrar, METHOD_VIEW_CHANNEL)
+    private val channel = MethodChannel(messenger, METHOD_VIEW_CHANNEL)
+    private val parentLinearLayout: LinearLayout = LinearLayout(context)
+    private lateinit var nameTextView: TextView
     private val defaultImage = "https://cdn-images-1.medium.com/max/280/1*X5PBTDQQ2Csztg3a6wofIQ@2x.png"
 
     init {
         channel.setMethodCallHandler(this)
 
-        val name = creationParams?.get("name") as? String
-        val role = creationParams?.get("role") as? String
-        val image = creationParams?.get("image") as? String
+        val name = args?.get("name") as? String
+        val role = args?.get("role") as? String
+        val image = args?.get("image") as? String
+        createProfileView(context, name ?: "...", role ?: "...", image ?: defaultImage)
+    }
 
+    private fun createProfileView(context: Context, name: String, role: String, image: String) {
         val inflater: LayoutInflater = LayoutInflater.from(context)
         val inflatedLayout: View = inflater.inflate(R.layout.profile, parentLinearLayout, false)
 
         nameTextView = inflatedLayout.findViewById(R.id.profile_name)
-        nameTextView.text = "${name ?: "..."}"
+        nameTextView.text = name
 
-        inflatedLayout.findViewById<TextView>(R.id.profile_role).text = "${role ?: "..."}"
+        inflatedLayout.findViewById<TextView>(R.id.profile_role).text = role
 
         Glide.with(context)
-                .load(image ?: defaultImage)
+                .load(image)
                 .circleCrop()
                 .into(inflatedLayout.findViewById(R.id.profile_image))
 
@@ -59,18 +61,17 @@ internal class NativeView(registrar: BinaryMessenger, context: Context, viewId: 
     override fun dispose() {}
 
     override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
-        when (call.method) {
-            SET_NAME_METHOD -> {
-                val name = call.arguments.toString()
-                if (name.isEmpty() || name.length < 5) {
-                    result.success(false)
-                    return
-                }
-                nameTextView.text = name;
-                result.success(true)
-            }
-            else -> result.notImplemented()
+        if (call.method != SET_NAME_METHOD) {
+            result.notImplemented()
+            return
         }
+        val name = call.arguments.toString()
+        if (name.isEmpty() || name.length < 5) {
+            result.success(false)
+            return
+        }
+        nameTextView.text = name
+        result.success(true)
     }
 
     override fun onAttachedToActivity(binding: ActivityPluginBinding) {
